f4e1e7858d10800c38cc80be1a9016fb6631d5ab
JMJ Riyana <jeemariyana@gmail.com>
2020-10-17 17:56:38 +0530

    navigationBAr

--- book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/App.js
+++ book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './App.css';
 
+import NavigationBar from './components/NavigationBar';
 function App() {
   return (
     <div className="App">
-   
-        Welcome to BookShop
-
+      <NavigationBar/>
+      <p>
+        Welcome to Book Shop
+      </p>
     </div>
   );
 }
---
+++ book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/NavigationBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class NavigationBar extends React.Component {
+    render() {
+        return (<div>Navigation Bar</div>);
+    }
+}
+
+export default NavigationBar;
