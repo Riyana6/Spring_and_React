b30535c80605516624d5d084d9a93aaf19f81330
JMJ Riyana <jeemariyana@gmail.com>
2020-10-17 23:23:24 +0530

    booklist

--- book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/BookList.js
+++ book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/BookList.js
@@ -1,11 +1,31 @@
-import React from'react';
-class BookList extends React.Component{
+import React, {Component} from'react';
+import {Card,Table} from 'react-bootstrap'
+export default class BookList extends Component{
     render() {
         return (
-            <div className="text-white">Book List
-            </div>
+            <Card className={"border border-dark bg-dark text-white"}>
+                <Card.Header>BookList</Card.Header>
+                <Card.Body>
+                    <Table striped bordered hover variant="dark">
+                        <thead>
+                            <tr> 
+                                <th>Title</th>
+                                <th>Aurthor</th>
+                                <th>ISBN Number</th>
+                                <th>Price</th>
+                                <th>Language</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr align="center">
+                                <td colSpan="6">No Books Available.</td>
+                            </tr>
+                           
+                        </tbody>
+                    </Table>
+                </Card.Body>
+            </Card>
         );
     }
 }
-
-export default BookList;
