1e9f0ed088a7f273b8e390d6931940f0e9a31439
JMJ Riyana <jeemariyana@gmail.com>
2020-10-17 20:29:50 +0530

    appja

--- book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/App.js
+++ book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row ,Jumbotron} from 'react-bootstrap';
 
 import './App.css';
 
@@ -7,9 +8,17 @@ function App() {
   return (
     <div className="App">
       <NavigationBar/>
-      <p>
-        Welcome to Book Shop
-      </p>
+      <Container>
+        <Row>
+          <Jumbotron>
+            <h1>Hello, world!</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.
+            </p>
+          </Jumbotron>
+        </Row>
+      </Container>
     </div>
   );
 }
--- book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/NavigationBar.js
+++ book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/NavigationBar.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Navbar, Nav} from 'react-bootstrap';
 class NavigationBar extends React.Component {
     render() {
         return (
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="/"><img alt="book" src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Book_icon_1.png" width="25" height="25"/>Book Shop</Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <img alt="book" src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Book_icon_1.png" width="25" height="25"/>Book Shop
+                </Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#">Add Book</Nav.Link>
+                    <Nav.Link href="#">Book List</Nav.Link>
+                </Nav>
             </Navbar>
         );
     }
