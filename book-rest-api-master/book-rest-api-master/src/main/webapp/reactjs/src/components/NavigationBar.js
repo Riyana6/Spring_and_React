c1e5808516a39970d423b47f6e8a1b9cfd04e30c
JMJ Riyana <jeemariyana@gmail.com>
2020-10-17 20:13:33 +0530

    navbarbook

--- book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/NavigationBar.js
+++ book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/NavigationBar.js
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import {Navbar} from 'react-bootstrap';
 class NavigationBar extends React.Component {
     render() {
-        return (<div>Navigation Bar</div>);
+        return (
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="/"><img alt="book" src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Book_icon_1.png" width="25" height="25"/>Book Shop</Navbar.Brand>
+            </Navbar>
+        );
     }
 }
 
