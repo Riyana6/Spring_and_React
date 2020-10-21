6de8078754805b4eef0e781760a667482e15c3fc
JMJ Riyana <jeemariyana@gmail.com>
2020-10-21 10:21:56 +0530

    userlist

--- book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/NavigationBar.js
+++ book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/NavigationBar.js
@@ -6,7 +6,7 @@ export default class NavigationBar extends Component {
         return (
             <Navbar bg="dark" variant="dark">
                 <Link to={""} className="navbar-brand">
-                    <img alt="book" src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Book_icon_1.png" width="25" height="25"/>Book Shop
+                    <img alt="brand" src="https://upload.wikimedia.org/wikipedia/commons/b/ba/Book_icon_1.png" width="25" height="25"/>Book Shop
                 </Link>
                 <Nav className="mr-auto">
                     <Link to={"add"} className="nav-link">Add Book</Link>
