92641a17ffa516e415f3f98a6f47c49956f789de
JMJ Riyana <jeemariyana@gmail.com>
2020-10-21 21:10:19 +0530

    bookjs

--- book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/BookList.js
+++ book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/BookList.js
@@ -1,125 +1,118 @@
-import React, {Component} from'react';
-import {Card,Table, Image, ButtonGroup , Button} from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Card, Table, Image, ButtonGroup, Button} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faList, faEdit,faTrash} from '@fortawesome/free-solid-svg-icons';
+import {faList, faEdit, faTrash} from '@fortawesome/free-solid-svg-icons';
+import {Link} from 'react-router-dom';
 import MyToast from './MyToast';
 import axios from 'axios';
-import {Link} from 'react-router-dom';
-
-export default class BookList extends Component{
-
+export default class BookList extends Component {
     constructor(props) {
         super(props);
         this.state = {
             books : []
         };
     }
-
     componentDidMount() {
         this.findAllBooks();
     }
 
-    findAllBooks(){
+    /*findAllBooks() {
         fetch("http://localhost:8081/rest/books")
-            .then(response => response.json)
+            .then(response => response.json())
             .then((data) => {
                 this.setState({books: data});
             });
-        };
+    };*/
 
-    /*findAllBooks(){
+    findAllBooks() {
         axios.get("http://localhost:8081/rest/books")
             .then(response => response.data)
             .then((data) => {
                 this.setState({books: data});
             });
-        };*/
+    };
 
-    deleteBook = (bookId) => {
-        fetch("http://localhost:8081/rest/books" + bookId, {
+    /*deleteBook = (bookId) => {
+        fetch("http://localhost:8081/rest/books/"+bookId, {
             method: 'DELETE'
         })
-        .then(response => response.json())    
+        .then(response => response.json())
         .then((book) => {
             if(book) {
-                this.setState({"show" : true});
-                setTimeout(() => this.setState({"show" : false}) , 3000);
+                this.setState({"show":true});
+                setTimeout(() => this.setState({"show":false}), 3000);
                 this.setState({
-                    books : this.state.books.filter(book => book.id !== bookId)
+                    books: this.state.books.filter(book => book.id !== bookId)
                 });
             } else {
                 this.setState({"show":false});
             }
         });
-    };
-    
+    };*/
 
-    /*deleteBook = (bookId) => {
-        axios.delete("http://localhost:8081/rest/books" + bookId)
+    deleteBook = (bookId) => {
+        axios.delete("http://localhost:8081/rest/books/"+bookId)
             .then(response => {
                 if(response.data != null) {
-                    this.setState({"show" : true});
-                    setTimeout(() => this.setState({"show" : false}) , 3000);
+                    this.setState({"show":true});
+                    setTimeout(() => this.setState({"show":false}), 3000);
                     this.setState({
-                        books : this.state.books.filter(book => book.id !== bookId)
+                        books: this.state.books.filter(book => book.id !== bookId)
                     });
                 } else {
                     this.setState({"show":false});
                 }
             });
-    };*/
-
+    };
     render() {
         return (
             <div>
-                <div style = {{"display":this.state.show ? "block" : "none"}}>
-                    <MyToast show = {this.state.show} message = {"Book Deleted Successfully"} type = {"danger"} />
+                <div style={{"display":this.state.show ? "block" : "none"}}>
+                    <MyToast show = {this.state.show} message = {"Book Deleted Successfully."} type = {"danger"}/>
                 </div>
                 <Card className={"border border-dark bg-dark text-white"}>
-                <Card.Header><FontAwesomeIcon icon={faList}/> BookList</Card.Header>
-                <Card.Body>
-                    <Table striped bordered hover variant="dark">
-                        <thead>
-                            <tr> 
-                                <th>Title</th>
-                                <th>Author</th>
-                                <th>ISBN Number</th>
-                                <th>Price</th>
-                                <th>Language</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.books.length === 0 ?
-                                <tr align="center">
-                                    <td colSpan="6">No Books Available.</td>
-                                </tr> :
-                                    this.state.books.map((book) => (
-                                <tr key={book.id}>
-                                    <td>
-                                        <Image src={book.coverPhotoURL} roundedCircle width="25" height="25"/>  {book.title}
-                                    </td>
-                                    <td>{book.author}</td>
-                                    <td>{book.isbnNumber}</td>
-                                    <td>{book.price}</td>
-                                    <td>{book.language}</td>
-                                    <td>
-                                        <ButtonGroup>
-                                            <Link to={"edit/".book.id} className="btn btn-sm btn-outline-primary"><FontAwesomeIcon icon={faEdit}/> Add Book</Link>{'  '}
-                                            <Button size="sm" variant="outline-danger" onClick={this.deleteBook.bind(this,book.id)}><FontAwesomeIcon icon={faTrash}/></Button>
-                                        </ButtonGroup>
-                                    </td>
+                    <Card.Header><FontAwesomeIcon icon={faList} /> Book List</Card.Header>
+                    <Card.Body>
+                        <Table bordered hover striped variant="dark">
+                            <thead>
+                                <tr>
+                                  <th>Title</th>
+                                  <th>Author</th>
+                                  <th>ISBN Number</th>
+                                  <th>Price</th>
+                                  <th>Language</th>
+                                  <th>Actions</th>
                                 </tr>
-                                ))
-                            }
-                           
-                        </tbody>
-                    </Table>
-                </Card.Body>
-            </Card>
+                              </thead>
+                              <tbody>
+                                {
+                                    this.state.books.length === 0 ?
+                                    <tr align="center">
+                                      <td colSpan="6">No Books Available.</td>
+                                    </tr> :
+                                    this.state.books.map((book) => (
+                                    <tr key={book.id}>
+                                        <td>
+                                            <Image src={book.coverPhotoURL} roundedCircle width="25" height="25"/> {book.title}
+                                        </td>
+                                        <td>{book.author}</td>
+                                        <td>{book.isbnNumber}</td>
+                                        <td>{book.price}</td>
+                                        <td>{book.language}</td>
+                                        <td>
+                                            <ButtonGroup>
+                                                <Link to={"edit/"+book.id} className="btn btn-sm btn-outline-primary"><FontAwesomeIcon icon={faEdit} /></Link>{' '}
+                                                <Button size="sm" variant="outline-danger" onClick={this.deleteBook.bind(this, book.id)}><FontAwesomeIcon icon={faTrash} /></Button>
+                                            </ButtonGroup>
+                                        </td>
+                                    </tr>
+                                    ))
+                                }
+                              </tbody>
+                        </Table>
+                    </Card.Body>
+                </Card>
             </div>
-            
         );
     }
 }
--- book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/UserList.js
+++ book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/UserList.js
@@ -1,10 +1,9 @@
-import React , {Component} from 'react';
-import {Card,Table, InputGroup, FormControl, Button} from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Card, Table, InputGroup, FormControl, Button} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faUsers, faStepForward,faStepBackward,faFastBackward,faFastForward} from '@fortawesome/free-solid-svg-icons';
+import {faUsers, faStepBackward, faFastBackward, faStepForward, faFastForward} from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
-
-export default class UserList extends Component{
+export default class UserList extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -13,33 +12,37 @@ export default class UserList extends Component{
             usersPerPage : 5
         };
     }
-
     componentDidMount() {
         this.findAllRandomUsers();
     }
 
-    findAllRandomUsers(){
+    /*findAllRandomUsers() {
+        fetch("https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole")
+            .then(response => response.json())
+            .then((data) => {
+                this.setState({users: data});
+            });
+    };*/
+
+    findAllRandomUsers() {
         axios.get("https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole")
             .then(response => response.data)
             .then((data) => {
                 this.setState({users: data});
             });
     };
-
     changePage = event => {
         this.setState({
             [event.target.name]: parseInt(event.target.value)
         });
     };
-
     firstPage = () => {
         if(this.state.currentPage > 1) {
             this.setState({
-                currentPage:1
+                currentPage: 1
             });
         }
     };
-
     prevPage = () => {
         if(this.state.currentPage > 1) {
             this.setState({
@@ -47,15 +50,13 @@ export default class UserList extends Component{
             });
         }
     };
-
     lastPage = () => {
         if(this.state.currentPage < Math.ceil(this.state.users.length / this.state.usersPerPage)) {
             this.setState({
-                currentPage:  Math.ceil(this.state.users.length / this.state.usersPerPage)
+                currentPage: Math.ceil(this.state.users.length / this.state.usersPerPage)
             });
         }
     };
-
     nextPage = () => {
         if(this.state.currentPage < Math.ceil(this.state.users.length / this.state.usersPerPage)) {
             this.setState({
@@ -63,15 +64,12 @@ export default class UserList extends Component{
             });
         }
     };
-
     render() {
-
         const {users, currentPage, usersPerPage} = this.state;
         const lastIndex = currentPage * usersPerPage;
         const firstIndex = lastIndex - usersPerPage;
-        const currentUsers = users.slice(firstIndex,lastIndex);
+        const currentUsers = users.slice(firstIndex, lastIndex);
         const totalPages = users.length / usersPerPage;
-
         const pageNumCss = {
             width: "45px",
             border: "1px solid #17A2B8",
@@ -79,15 +77,12 @@ export default class UserList extends Component{
             textAlign: "center",
             fontWeight: "bold"
         };
-
         return (
             <div>
                 <Card className={"border border-dark bg-dark text-white"}>
-                    <Card.Header>
-                        <FontAwesomeIcon icon={faUsers}/> UserList
-                    </Card.Header>
+                    <Card.Header><FontAwesomeIcon icon={faUsers} /> User List</Card.Header>
                     <Card.Body>
-                        <Table striped bordered hover variant="dark">
+                        <Table bordered hover striped variant="dark">
                             <thead>
                                 <tr>
                                     <td>Name</td>
@@ -99,13 +94,12 @@ export default class UserList extends Component{
                             </thead>
                             <tbody>
                                 {users.length === 0 ?
-                                    <tr align = "center">
-                                        <td colSpan = "6"> No Users Available
-                                        </td>
+                                    <tr align="center">
+                                        <td colSpan="6">No Users Available</td>
                                     </tr> :
                                     currentUsers.map((user, index) => (
-                                        <tr key ={index}>
-                                            <td>{user.first}{'  '}{user.last}</td>
+                                        <tr key={index}>
+                                            <td>{user.first}{' '}{user.last}</td>
                                             <td>{user.email}</td>
                                             <td>{user.address}</td>
                                             <td>{user.created}</td>
@@ -116,38 +110,40 @@ export default class UserList extends Component{
                             </tbody>
                         </Table>
                     </Card.Body>
-                    <Card.Footer>
-                        <div style ={{"float" : "left"}}>
-                            Showing Page {currentPage} of {totalPages}
-                        </div>
-                        <div style ={{"float" : "right"}}>
-                            <InputGroup size = "sm">
-                                <InputGroup.Prepend>
-                                    <Button type = "button" variant="outline-info" disabled = {currentPage === 1 ? true : false}
-                                        onClick={this.firstPage}>
-                                        <FontAwesomeIcon icon={faFastBackward}/> First
-                                    </Button>
-                                    <Button type = "button" variant="outline-info" disabled = {currentPage === 1 ? true : false}
-                                        onClick={this.prevPage}>
-                                        <FontAwesomeIcon icon={faStepBackward}/> Prev
-                                    </Button>
-                                </InputGroup.Prepend>
-                                <FormControl style={pageNumCss} className={"bg-dark"} name="currentPage" value={currentPage}
-                                    onChange={this.changePage}/>
-                                <InputGroup.Append> 
-                                    <Button type = "button" variant="outline-info" disabled = {currentPage === totalPages ? true : false}
-                                        onClick={this.nextPage}>
-                                        <FontAwesomeIcon icon={faStepForward}/> Next
-                                    </Button>
-                                    <Button type = "button" variant="outline-info" disabled = {currentPage === totalPages ? true : false}
-                                        onClick={this.lastPage}>
-                                        <FontAwesomeIcon icon={faFastForward}/> Last
-                                    </Button>
-                                </InputGroup.Append>
-                            </InputGroup>
-                        </div>
-                    </Card.Footer>
-                </Card>   
+                    {users.length > 0 ?
+                        <Card.Footer>
+                            <div style={{"float":"left"}}>
+                                Showing Page {currentPage} of {totalPages}
+                            </div>
+                            <div style={{"float":"right"}}>
+                                <InputGroup size="sm">
+                                    <InputGroup.Prepend>
+                                        <Button type="button" variant="outline-info" disabled={currentPage === 1 ? true : false}
+                                            onClick={this.firstPage}>
+                                            <FontAwesomeIcon icon={faFastBackward} /> First
+                                        </Button>
+                                        <Button type="button" variant="outline-info" disabled={currentPage === 1 ? true : false}
+                                            onClick={this.prevPage}>
+                                            <FontAwesomeIcon icon={faStepBackward} /> Prev
+                                        </Button>
+                                    </InputGroup.Prepend>
+                                    <FormControl style={pageNumCss} className={"bg-dark"} name="currentPage" value={currentPage}
+                                        onChange={this.changePage}/>
+                                    <InputGroup.Append>
+                                        <Button type="button" variant="outline-info" disabled={currentPage === totalPages ? true : false}
+                                            onClick={this.nextPage}>
+                                            <FontAwesomeIcon icon={faStepForward} /> Next
+                                        </Button>
+                                        <Button type="button" variant="outline-info" disabled={currentPage === totalPages ? true : false}
+                                            onClick={this.lastPage}>
+                                            <FontAwesomeIcon icon={faFastForward} /> Last
+                                        </Button>
+                                    </InputGroup.Append>
+                                </InputGroup>
+                            </div>
+                        </Card.Footer> : null
+                     }
+                </Card>
             </div>
         );
     }
