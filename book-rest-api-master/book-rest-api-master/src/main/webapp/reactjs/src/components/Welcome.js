e3c5af9dab482bae187e53aabafa854d406820a1
JMJ Riyana <jeemariyana@gmail.com>
2020-10-21 19:28:48 +0530

    welcome

--- book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/Welcome.js
+++ book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/components/Welcome.js
@@ -1,20 +1,18 @@
-import React ,{Component} from'react';
+import React from'react';
 import { Jumbotron} from 'react-bootstrap';
 
-export default function Welcome extends Component {
-    render() {
-        return (
-            <Jumbotron className="bg-dark text-white">
-                <h1>Welcome to Book Shop</h1>
-                <blockquote className="blockquote mb-0">
-                    <p>
-                        Good Friends, Good Books, and a sleepy conscience: this is the ideal life. 
-                    </p>
-                    <footer className="blockquote-footer">
-                        Jeema Riyana
-                    </footer>
-                </blockquote>
-            </Jumbotron>
-        );
-    }
+export default function Welcome() {
+    return (
+        <Jumbotron className="bg-dark text-white">
+            <h1>Welcome to Book Shop</h1>
+            <blockquote className="blockquote mb-0">
+                <p>
+                    Good Friends, Good Books, and a sleepy conscience: this is the ideal life. 
+                </p>
+                <footer className="blockquote-footer">
+                    Jeema Riyana
+                </footer>
+            </blockquote>
+        </Jumbotron>
+    );
 }
