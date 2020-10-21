13e71b44fe4ac3411e0e44bcd1e8db46758bde6e
JMJ Riyana <jeemariyana@gmail.com>
2020-10-21 19:38:35 +0530

    appjs

--- book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/App.js
+++ book-rest-api-master/book-rest-api-master/src/main/webapp/reactjs/src/App.js
@@ -27,7 +27,7 @@ export default function App() {
         <Row>
           <Col lg={12} style={marginTop}>
             <Switch>
-              <Route path="/" exact component={() => <Welcome heading={} desc={}/>}/>
+              <Route path="/" exact component={() => <Welcome heading={heading} desc={desc}/>}/>
               <Route path="/add" exact component={Book}/>
               <Route path="/edit/:id" exact component={Book}/>
               <Route path="/list" exact component={BookList}/>
