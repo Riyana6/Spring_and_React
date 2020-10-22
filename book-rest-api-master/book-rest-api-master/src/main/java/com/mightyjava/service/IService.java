4179e88c132857f6462fc4509f0aaee993bb9a0f
JMJ Riyana <jeemariyana@gmail.com>
2020-10-22 15:31:25 +0530

    Iservice

--- book-rest-api-master/book-rest-api-master/src/main/java/com/mightyjava/service/IService.java
+++ book-rest-api-master/book-rest-api-master/src/main/java/com/mightyjava/service/IService.java
@@ -1,16 +1,13 @@
 package com.mightyjava.service;
 
-import java.util.Collection;
-import java.util.Optional;
-
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IService<T> {
 	Page<T> findAll(Pageable pageable);
-	
-	Optional<T> findById(Long id);
-	
+
+	T findById(Long id);
+
 	T saveOrUpdate(T t);
 	
 	String deleteById(Long id);
